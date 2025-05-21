a72bfd0b565b3cdbfbfeccdfdf38524ab5df7d22
Ohne-Dich <lawerenz.f@gmail.com>
2025-05-21 11:58:06 +0200

    added REPL a bit

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		txt := cleanInput(scanner.Text())
+		msg := fmt.Sprintf("Your command was: %v", txt[0])
+		fmt.Println(msg)
+	}
 }
